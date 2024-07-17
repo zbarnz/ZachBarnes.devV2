4e92291820814deee533bd518001807673ff851e
Zach Barnes <zbarnz@yahoo.com>
2024-07-16 21:41:55 -0600

    update deps for vercel analytics, add vercel analytics component

--- src/App.js
+++ src/App.js
@@ -10,12 +10,13 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
-  Navigate
+  Navigate,
 } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const [load, upadateLoad] = useState(true);
@@ -31,6 +32,8 @@ function App() {
   return (
     <Router>
       <Preloader load={load} />
+      <Analytics />
+      import {Analytics} from "@vercel/analytics/react"
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
         <ScrollToTop />
@@ -39,7 +42,7 @@ function App() {
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
-          <Route path="*" element={<Navigate to="/"/>} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
       </div>
